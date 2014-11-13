94226a4f7b2e395cd36bb66620f3d042c0802344
XJMa <xjma1123@gmail.com>
2014-11-13 15:48:30 -0500

    environment start as gray

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 
 public class Obstacle : MonoBehaviour {
-	public LensColor color;
+	public LensColor color;//need to be assigned in unity
+	public Material myMat;//need to be assigned in unity
+	private GameManager gameManager;
 	// Use this for initialization
 	void Start () {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		collider.enabled = false;
+		//set start transparency
+		Color c = renderer.material.color;
+		c.a = 0.8f;
+		renderer.material.color = c;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(gameManager.lens != LensColor.none){
+			renderer.material = myMat;
+			collider.enabled = true;
+		}	
 	
 	}
 }
