89b569dab131292bab32434f48be60433b606b0d
XJMa <xjma1123@gmail.com>
2014-11-16 00:23:26 -0500

    merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,21 +5,21 @@ public class GameManager : MonoBehaviour {
 	public GameObject light;
 
 	public LensColor lens;
-	public bool hasRedLens, hasYellowLens, hsBlueLens;
+	public bool hasRedLens, hasYellowLens, hasBlueLens;
 	// Use this for initialization
 	void Start () {
 		lens = LensColor.none;
 		hasRedLens = true;//initiate all lens as true for test
 		hasYellowLens = true;
-		hasRedLens = true;
+		hasBlueLens = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//switch lenses
 		if(Input.GetKeyDown(KeyCode.Alpha1) && hasRedLens) lens = LensColor.red;
-		if(Input.GetKeyDown(KeyCode.Alpha2) && hasRedLens) lens = LensColor.yellow;
-		if(Input.GetKeyDown(KeyCode.Alpha3) && hasRedLens) lens = LensColor.blue;
+		if(Input.GetKeyDown(KeyCode.Alpha2) && hasYellowLens) lens = LensColor.yellow;
+		if(Input.GetKeyDown(KeyCode.Alpha3) && hasBlueLens) lens = LensColor.blue;
 		if(lens == LensColor.red){
 			light.light.color = Color.Lerp(Color.red, Color.white, 0.5f);
 		}
