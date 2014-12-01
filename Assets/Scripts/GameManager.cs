f5622c343937ebff6c3b51f06a2fefb00dd7c208
XJMa <xjma1123@gmail.com>
2014-11-30 22:05:15 -0500

    slippy control fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,18 @@ public class GameManager : MonoBehaviour {
 	public LensColor lens;
 	public bool hasRedLens, hasYellowLens, hasBlueLens;
 	public Level level;  //Change in unity inspector
+	public Texture2D redImage; 
+	public Texture2D blueImage; 
+	public Texture2D yellowImage; 
+	public Font font; 
+	public GUIStyle style; 
+
+	bool stopRoutine;
+
+	int i = 0; 
 	// Use this for initialization
-	public Level nextLevel;
+	public Level nextLevel; 
+
 	void Start () {
 		lens = LensColor.none;
 		if( level == Level.level0 ){
@@ -32,6 +42,18 @@ public class GameManager : MonoBehaviour {
 			lens = LensColor.blue;
 			nextLevel = Level.higherlevels;
 		}
+
+		gameObject.transform.position = new Vector3 (0.54f, 0.89f, 0.0f); 
+		gameObject.transform.localScale = new Vector3 (0.01f, 0.01f, 1.0f);
+
+		guiTexture.pixelInset = new Rect(0, 0, 30, 40); 
+
+		style.font = font;
+		style.normal.textColor = Color.white;
+
+		stopRoutine = false;
+
+		
 	}
 	
 	// Update is called once per frame
@@ -42,13 +64,94 @@ public class GameManager : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Alpha3) && hasYellowLens) lens = LensColor.yellow;
 		if(lens == LensColor.red){
 			light.light.color = Color.Lerp(Color.red, Color.white, 0.5f);
+			guiTexture.texture = redImage;
 		}
 		if(lens == LensColor.yellow){
 			light.light.color = Color.Lerp(Color.yellow, Color.white, 0.5f);
+			guiTexture.texture = yellowImage;
 		}
 		if(lens == LensColor.blue){
 			light.light.color = Color.Lerp(Color.blue, Color.white, 0.5f);
+			guiTexture.texture = blueImage;
 		}
+
+		if(level == Level.level0){
+			if(stopRoutine == false){
+				StartCoroutine(Sequence(ShowMessage("WELCOME TO THE FIRST TUTORIAL LEVEL", 2),
+		               			ShowMessage("Grab the Red Lens to interact with the environment", 2),
+		                        ShowMessage("When you use a lens, you can see everything but the objects of that color", 2)));
+			
+				stopRoutine = true;
+			}	
 		
+		}
+
+		if(level == Level.level1){
+			if(stopRoutine == false){
+				StartCoroutine(Sequence(ShowMessage("Grab the Blue Lens", 2),
+			                        ShowMessage("Remember, you can see everything except the \nobjects of the color of the lens you are using", 2)));
+				stopRoutine = true;
+			}
+		}
+		if(level == Level.level2){
+			if(stopRoutine == false){
+			StartCoroutine(Sequence(ShowMessage("Grab the Yellow Lens", 2),
+			                        ShowMessage("Play with the three lenses to alternate the obstacles you can see. \nHave fun!", 2)));
+			
+
+				stopRoutine = true;
+			}
+			
+		}
+		Player player = GameObject.Find("Player").GetComponent<Player>();
+		if (player.win){
+			StartCoroutine(WinMessage ("CONGRATS! You made it!", 1));
+		}
+
+
+
+	}
+	void OnGUI(){
+		if(level == Level.level0){
+			if(hasRedLens){
+
+				GUILayout.BeginArea(new Rect(Screen.width * 0.01f, Screen.width * 0.05f, 1000, 1000));
+				GUILayout.Label("Shows you what lenses you have", style);
+				GUILayout.EndArea ();
+
+				GUILayout.BeginArea(new Rect(Screen.width * 0.5f, Screen.width * 0.05f, 1000, 1000));
+				GUILayout.Label("Shows you which lens is activated", style);
+				GUILayout.EndArea ();
+			}
+		}
+	}
+
+
+	public static IEnumerator Sequence(params IEnumerator[] sequence)
+	{
+		for(int i = 0 ; i < sequence.Length; ++i)
+		{
+			while(sequence[i].MoveNext())
+				yield return sequence[i].Current;
+		}
+	}
+
+	IEnumerator ShowMessage (string message, float delay) {
+		guiText.text = message;
+		guiText.font = font;
+		guiText.pixelOffset = new Vector2 (-120, -300);
+		guiText.enabled = true;
+		yield return new WaitForSeconds(delay);
+		guiText.enabled = false;
+	}
+
+	IEnumerator WinMessage (string message, float delay) {
+		guiText.text = message;
+		guiText.font = font;
+		guiText.fontSize = 30; 
+		guiText.pixelOffset = new Vector2 (-120, -300);
+		guiText.enabled = true;
+		yield return new WaitForSeconds(delay);
+		Application.LoadLevel("Win");
 	}
 }
