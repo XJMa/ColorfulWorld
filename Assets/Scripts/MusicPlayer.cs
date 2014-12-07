f5c711573189c0a75842cda6af48567c02040b35
jojojangles <jcotosax@gmail.com>
2014-12-07 13:52:34 -0500

    Added persistent theme music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	AudioSource theme;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+		AudioSource[] sources = GetComponents<AudioSource>();
+		theme = sources[0];
+		theme.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
